a7be7762f29fa720db30e259dcbde19d8781a879
Tazz <45635565+tazz1234@users.noreply.github.com>
2018-12-05 18:15:17 +0100

    Initial commit

---
+++ WoerstsNumberOne/Assets/HansWoerstController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HansWoerstController : MonoBehaviour {
+    /// <summary>
+    /// Halo zusammen
+    /// </summary>
+    // Use this for initialization
+
+    int[] array = new int[10];
+	void Start ()
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Init Woerst");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while (true)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ WoerstsNumberOne/Library/Collab/Original/Assets/HansWoerstController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HansWoerstController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Init Woerst");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while (true)
+        {
+            Debug.Log("Test"); Debug.Log("Es kann nur einen geben!");
+        }
+    }
+}
